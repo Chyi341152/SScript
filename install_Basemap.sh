ff6b0954f4908a3c88b7ad0924ea6b6313862b67
ChyiYaqing <chyiyaqing@gmail.com>
2018-01-12 09:33:01 +0800

    install basemap packages, allows us to create maps using python

---
+++ install_Basemap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Matplotlib Basemap Toolkit documentation
+# The matplotlib basemap toolkit is a library for plotting 2D data on maps in Python.
+# install basemap quickly on ubuntu
+sudo apt-get update
+sudo apt-get install libgeos
+sudo apt-get install libgeos-dev
+pip install --user https://github.com/matplotlib/basemap/archive/master.zip
